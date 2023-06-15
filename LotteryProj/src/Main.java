9c8cb325abdd1e54f6b381f5fdf2acb800c882db
Class01 <Class01@DESKTOP-9H6B9C5>
2023-06-15 16:29:21 +0900

    array size

--- LotteryProj/src/Main.java
+++ LotteryProj/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
 	// 1 ~ 23
 	public static void main(String[] args) {
-		int STU_NUM = 23;
-		int[] n = new int[STU_NUM];
 		String[] stu_name = { "강준형", "강지윤", "공나은", "김강연", "김미정", "김사희",
 				"김현규", "김형순", "박태호", "박하영", "손승범", "손유미", "송병권",
 				"신우철", "이은영", "전예서", "정가영", "정세연", "정예은", "정웅기",
 				"정하영", "정하원", "황다원", };
+		int[] n = new int[stu_name.length];
 		boolean b = true;
+		
 		while (b) {
-			int r = (int) (Math.random() * (STU_NUM));
+			int r = (int) (Math.random() * (stu_name.length));
 
 			if (n[r] == 0) {
 				System.out.printf("%2d : %s\n", (r + 1), stu_name[r]);
@@ -17,7 +17,7 @@ public class Main {
 			}
 
 			boolean c = true;
-			for (int i = 0; i < STU_NUM; i++) {
+			for (int i = 0; i < stu_name.length; i++) {
 				if (n[i] == 0) {
 					c = false;
 					break;
